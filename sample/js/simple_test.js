44f1a9be539914d8b65a20b946e130a09905b573
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-09-30 02:34:21 +0900

    Fix: 重複したconstの変数定義

---
+++ sample/js/simple_test.js
@@ -0,0 +1,138 @@
+const Versalog = require("versalog");
+
+// show_file false
+const logger = new Versalog();
+logger.setConfig({
+  show_file: false,
+  show_tag: false,
+  notice: false,
+  tag: null,
+  mode: "simple",
+});
+
+logger.info("情報ログ");
+logger.error("エラーログ");
+logger.warning("警告ログ");
+logger.debug("デバッグログ");
+logger.critical("クリティカルログ");
+
+// show_file true
+logger.setConfig({
+  show_file: true,
+  show_tag: false,
+  notice: false,
+  tag: null,
+  mode: "simple",
+});
+
+logger.info("情報ログ");
+logger.error("エラーログ");
+logger.warning("警告ログ");
+logger.debug("デバッグログ");
+logger.critical("クリティカルログ");
+
+// show_tag False
+logger.setConfig({
+  show_file: false,
+  show_tag: false,
+  notice: false,
+  tag: null,
+  mode: "simple",
+});
+
+logger.info("情報ログ");
+logger.error("エラーログ");
+logger.warning("警告ログ");
+logger.debug("デバッグログ");
+logger.critical("クリティカルログ");
+
+// show_tag true
+logger.setConfig({
+  show_file: false,
+  show_tag: true,
+  notice: false,
+  tag: "VersaLog",
+  mode: "simple",
+});
+
+logger.info("情報ログ");
+logger.error("エラーログ");
+logger.warning("警告ログ");
+logger.debug("デバッグログ");
+logger.critical("クリティカルログ");
+
+// notice False
+logger.setConfig({
+  show_file: false,
+  show_tag: false,
+  notice: false,
+  tag: null,
+  mode: "simple",
+});
+
+logger.info("情報ログ");
+logger.error("エラーログ");
+logger.warning("警告ログ");
+logger.debug("デバッグログ");
+logger.critical("クリティカルログ");
+
+// notice true
+logger.setConfig({
+  show_file: false,
+  show_tag: false,
+  notice: true,
+  tag: null,
+  mode: "simple",
+});
+
+logger.info("情報ログ");
+logger.error("エラーログ");
+logger.warning("警告ログ");
+logger.debug("デバッグログ");
+logger.critical("クリティカルログ");
+
+// silent False
+logger.setConfig({
+  show_file: false,
+  show_tag: false,
+  notice: true,
+  silent: false,
+  tag: null,
+  mode: "simple",
+});
+
+logger.info("情報ログ");
+logger.error("エラーログ");
+logger.warning("警告ログ");
+logger.debug("デバッグログ");
+logger.critical("クリティカルログ");
+
+// silent False
+logger.setConfig({
+  show_file: false,
+  show_tag: false,
+  notice: true,
+  silent: true,
+  all_save: true,
+  tag: null,
+  mode: "simple",
+});
+
+logger.info("情報ログ");
+logger.error("エラーログ");
+logger.warning("警告ログ");
+logger.debug("デバッグログ");
+logger.critical("クリティカルログ");
+
+// enable_all true
+logger.setConfig({
+  enable_all: true,
+  tag: "VersaLog",
+  mode: "simple",
+});
+
+logger.info("情報ログ");
+logger.error("エラーログ");
+logger.warning("警告ログ");
+logger.debug("デバッグログ");
+logger.critical("クリティカルログ");
